f3127614187c1d223f8208063a7ce529bf7f1621
Klara <klara1@Klaras-MacBook-Pro.local>
2017-12-07 13:36:02 +0000

    BWP: Highlight previous meetups

--- page-meetup.php
+++ page-meetup.php
@@ -38,6 +38,39 @@ get_header(); ?>
                         <?php if( $wp_query->have_posts() ): ?>
                             <div class="meetup-listing-wrapper">
                                 <ul class="meetup-listing">
+                                    <li class="meetup-listing-next-event">
+                                        <?php $the_query = new WP_Query( 'posts_per_page=1' ); ?>
+                                        <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+                                            <div class="meetup-listing-image meetup-listing-img-<?php global $post; echo $post->post_name; ?>">
+                                                <?php if (has_post_thumbnail( $post->ID ) ): ?>
+                                                    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+
+                                                    <style>
+                                                        .meetup-listing-img-<?php global $post; echo $post->post_name; ?> {
+                                                            background-image: url('<?php echo $image[0]; ?>');
+                                                        }
+                                                    </style>
+                                                    <div class="meetup-listing-image-inner"></div>
+                                                <?php else: ?>
+                                                    <div class="meetup-listing-image-inner"></div>
+                                                <?php endif; ?>
+                                            </div>
+                                            <div class="meetup-listing-info">
+                                                <div class="meetup-listing-header">
+                                                    <span class="meetup-listing-date"><?php the_field('event_date'); ?></span>
+                                                    <span class="meetup-listing-title">
+                                                        <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                                                    </span>
+                                                </div>
+                                                <div class="talk-text">
+                                                    <?php the_content(); ?>
+                                                </div>
+                                            </div>
+                                        <?php endwhile; 
+                                        wp_reset_postdata(); ?>
+                                    </li>
+
+                                    <h3 class="meetup-listing-heading">Our Previous Meetups</h3>
                                     <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
                                     <li <?php post_class(); ?>>
                                         <div class="meetup-listing-image meetup-listing-img-<?php global $post; echo $post->post_name; ?>">
--- sidebar-home.php
+++ sidebar-home.php
@@ -1,11 +1,21 @@
 <aside class="sidebar">
     <div class="search-sidebar"><?php get_search_form(); ?></div>
-    <h3>Our Meetups</h3>
-    <?php $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>13)); ?>
-    <?php if ( $wpb_all_query->have_posts() ) : ?>
+    <h3>Previous Meetups</h3>
+    <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
+
+    <?php $args = array(
+        'post_type'      => 'post',
+        'post_status'    => 'publish',
+        'posts_per_page' =>  14,
+        'paged' => $paged
+    );
+
+    $wp_query = new WP_Query( $args ); ?>
+
+    <?php if ( $wp_query->have_posts() ) : ?>
         <ul class="meetup-list">
-            <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
-                <li>
+            <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+                <li <?php post_class(); ?>>
                     <span class="meetup-list-date"><?php the_field('event_date'); ?></span>
                     <span class="meetup-list-link">
                         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
