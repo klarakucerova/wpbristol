c9c1f881be0174a8a26180384f30c5e516ee990a
Klara <klara1@Klaras-MacBook-Pro.local>
2017-12-07 14:13:41 +0000

    BWP: Add favicon

--- header.php
+++ header.php
@@ -16,6 +16,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
 
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600,700|Ubuntu+Condensed" rel="stylesheet">
 
     <?php wp_head(); ?>
